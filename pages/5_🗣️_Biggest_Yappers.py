deb89e98a6b174b499521147c8c07bdee8e1463d
Bourkies <115338150+Bourkies@users.noreply.github.com>
2025-06-12 23:48:08 +1000

    Update 5_🗣️_Biggest_Yappers.py

--- pages/5_🗣️_Biggest_Yappers.py
+++ pages/5_🗣️_Biggest_Yappers.py
@@ -4,7 +4,7 @@
 import streamlit as st
 import pandas as pd
 import random
-import utils
+import Streamlit_utils
 import dashboard_texts as texts
 
 def display_yapper_leaderboard(df, period_suffix, title, messages, icon):
